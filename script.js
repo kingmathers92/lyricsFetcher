eb2053686d968221ea7d61059deae7c354477653
khaled <khaledb.yahya@gmail.com>
2022-04-15 00:17:06 +0100

    changed input icon

--- script.js
+++ script.js
@@ -34,5 +34,32 @@ btn.addEventListener('click', () => {
 
             document.querySelector('.lyrics-output').style.opacity = "1";
         });
+
+        function animateText(output) {
+            let text = output.value;
+            let to = text.length,
+              from = 0;
+
+            animate({
+              duration: 5000,
+              timing: bounce,
+              draw: function(progress) {
+                let result = (to - from) * progress + from;
+                output.value = text.substr(0, Math.ceil(result))
+              }
+            });
+          }
+
+
+          function bounce(timeFraction) {
+            for (let a = 0, b = 1; 1; a += b, b /= 2) {
+              if (timeFraction >= (7 - 4 * a) / 11) {
+                return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+              }
+            }
+          }
     }
 });
+
+
+
